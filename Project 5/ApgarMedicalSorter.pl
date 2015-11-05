c0863b868264548ab74ba803f2338b59e820839c
Kyle Flynn <kyle.flynn5798@yahoo.com>
2015-11-05 15:07:20 -0500

    Updated Projecy 5

--- Project 5/ApgarMedicalSorter.pl
+++ Project 5/ApgarMedicalSorter.pl
@@ -11,6 +11,7 @@ my @data;
 use constant DATA_FILE_1_IN => "./ApgarMedicalBest.txt";
 use constant DATA_FILE_2_IN => "./ApgarMedicalCushing.txt";
 use constant DATA_FILE_OUT => "./ApgarMedicalCombined.txt";
+use constant BIRTH_INDEX => 2;
 use constant COLUMNS => 3;
 
 sub main() {
@@ -50,7 +51,6 @@ sub readData() {
 
 sub modifyData() {
     #In here, organize the data some miraculous way || i = rows, j = columns
-    use constant AGE_INDEX => 2;
     
     my $size = @data;
     my $tempVar = 0;
@@ -60,7 +60,7 @@ sub modifyData() {
         $didSwap = 0; 
         for (my $i = 0; $i < $size; $i++) {            
             if(defined($data[$i + 1])) {
-                if($data[$i +1][AGE_INDEX] < $data[$i][AGE_INDEX]) {
+                if($data[$i +1][BIRTH_INDEX] < $data[$i][BIRTH_INDEX]) {
                     for (my $j = 0; $j < COLUMNS; $j++) {
                         $tempVar = $data[$i + 1][$j];
                         $data[$i +1][$j] = $data[$i][$j];
@@ -75,11 +75,28 @@ sub modifyData() {
 }
 
 sub printData() {
+    my @yearCollection = ([],[]);
     my $size = @data;
+    my $yearSize = 0;;
+    my $yearCount = 0;
     for (my $i = 0; $i < $size; $i++) {
-        for (my $j = 0; $j < COLUMNS; $j++) {
-            print "$data[$i][$j]\n";
+        $yearSize++;
+        if(defined($data[$i + 1])) {
+            if($data[$i][BIRTH_INDEX] != $data[$i + 1][BIRTH_INDEX]) {
+                $yearCount = 1;
+                $yearCollection[$yearSize] = $data[$i][BIRTH_INDEX];
+                $yearCollection[$yearSize][0] = $yearCount;
+            } else {
+                $yearCount++;
+                $yearSize--;
+                $yearCollection[$yearSize] = $data[$i][BIRTH_INDEX];
+                $yearCollection[$yearSize][0] = $yearCount;
+                print "$yearCount\n";
+            }
         }
+    }
+    for(my $i = 0; $i < $yearSize; $i++) {
+        print "$yearCollection[$i]: $yearCollection[$i][0]\n";
     }
 }
 
@@ -92,7 +109,7 @@ sub writeData() {
 			if ($j == COLUMNS - 1) {
 				print ($OUT "$data[$i][$j]");
 			} else {
-				#print ($OUT "$data[$i][$j],");
+				print ($OUT "$data[$i][$j],");
 			}
 		}
 		print ($OUT "\n");
